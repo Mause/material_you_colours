da5b9225ce2d1301442ee151e4892630f00712c7
Elliana <me@mause.me>
2021-11-21 20:08:37 +0800

    Build out example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
-import 'package:material_you_colours/material_you_colours.dart';
+import 'package:flutter/material.dart';
+import 'package:material_you_colours/material_you_colours.dart' show getMaterialYouThemeData;
 
 void main() {
   runApp(const MyApp());
@@ -16,15 +15,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  ThemeData? materialYouThemeData;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    var materialYouThemeData = await getMaterialYouThemeData();
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      this.materialYouThemeData = materialYouThemeData;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    getMaterialYouThemeData();
     return MaterialApp(
+      theme: materialYouThemeData,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('Running on: $materialYouThemeData\n'),
         ),
       ),
     );
